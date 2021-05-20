4e6bd49a015179ec3bacf1e84106d65bdcbe9e2c
sonu-kumar2001 <sonurocckkerr@gmail.com>
2021-05-20 22:41:30 +0530

    Added reusable components

---
+++ app/javascript/src/components/PageLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageLoader = () => {
+  return (
+    <button type="button" classNameName="bg-bb-purple-600 ..." disabled>
+      <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+      Loading
+    </button>
+  );
+};
+
+export default PageLoader;
